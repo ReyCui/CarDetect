329ca6efb7e2ccceb2c9bdc6edeeff0aa7eebb27
ReyCui <476840723@qq.com>
2024-04-22 09:31:45 +0800

    Update cars_train.py

--- cars_train.py
+++ cars_train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
 
-    # model = YOLO(model="yolov8n.yaml")
-    model = YOLO(model="./runs/detect/train/weights/best.pt")
+    model = YOLO(model="yolov8n.yaml")
+    # model = YOLO(model="./runs/detect/train/weights/best.pt")
 
-    results = model.train(data='cars.yaml', epochs=200, imgsz=512, batch=64)
+    results = model.train(data='cars_v3.yaml', epochs=300, imgsz=640, batch=64)
